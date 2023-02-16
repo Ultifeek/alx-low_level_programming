d7651227e8085131dff0533a75217053602fb167
Ultifeek <ultifeek1@gmail.com>
2023-02-16 18:26:48 +0000

    To print sentence without printf or puts followed by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<unistd.h>
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, msg, 60);
+	return (0);
+}
