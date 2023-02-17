f5e76e4b919010b834af754f7373799cc168d717
Ultifeek <ultifeek1@gmail.com>
2023-02-17 23:31:48 +0000

    To print all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * main - print the hexadecimal
+ * main - print digits 0 to 9 with comma and space
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -11,10 +11,11 @@ int main(void)
 	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
-	}
-	for (i = 97; i < 103; i++)
-	{
-		putchar(i);
+		if (i < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
