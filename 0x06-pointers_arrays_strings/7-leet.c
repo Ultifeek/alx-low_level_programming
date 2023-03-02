6179389ddbccfc03c2fd33106bc509474059fcfa
Ultifeek <ultifeek1@gmail.com>
2023-03-02 21:01:03 +0000

    function that encodes a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string
+ * @s: input string
+ * Return: encode string
+ */
+
+char *leet(char *s)
+{
+	int count = 0, i;
+	int lower[] = {97, 101, 111, 116, 108};
+	int cap[] = {65, 69, 79, 84, 76};
+	int nums[] = {52, 51, 48, 55, 49};
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*(s + count) == lower[i] || *(s + count) == cap[i])
+			{
+				*(s + count) = nums[i];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (s);
