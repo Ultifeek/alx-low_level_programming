67665b60e4426ebb72c335f081580b12235f6d08
Ultifeek <ultifeek1@gmail.com>
2023-04-04 13:56:52 +0000

    function that prints all the elements of a listint_t list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t n_nodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		nnodes++;
+		n_nodes++;
 	}
-	return (nnodes);
+	return (n_nodes);
 }
 
